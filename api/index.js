6fd0a5cf44a167ded0c181dfb49fd96ab3499c5a
Anas Abbas <anasabbasmalik88@gmail.com>
2024-10-29 10:47:18 +0500

    created and added OAuth functionality

--- api/index.js
+++ api/index.js
@@ -1,44 +1,38 @@
 import dotenv from "dotenv";
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
-import userRouter from "./routers/user.router.js"
-import signUpRouter from "./routers/auth.route.js"
+import userRouter from "./routers/user.router.js";
+import signUpRouter from "./routers/auth.route.js";
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log("Connected to MongoDb");   
-})
-.catch((err)=>{
-    console.log(err);
-    
-})
-
+    .then(() => {
+        console.log("Connected to MongoDb");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
-app.use(express.json())
+
+app.use(express.json());
 
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', signUpRouter);
 
-app.listen(3000 , ()=> {
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
-    
-})
+});
 
-
-app.use('/api/user' , userRouter);
-app.use('/api/auth' , signUpRouter )
-
-
-app.use((err,re,res,next)=>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error'
+    const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-        success : false,
+        success: false,
         statusCode,
         message
     });
 });
-

a58bec2e2128b5fe4f720f68b4fc521437639b1c
Anas Abbas <anasabbasmalik88@gmail.com>
2024-11-11 11:44:34 +0500

    Completed delete user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,10 +28,9 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
-  const [showListingsError , setShowListingsError] = useState(false)
-  const [userListings , setUserListings] = useState([])
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
-  
 
   useEffect(() => {
     if (file) {
@@ -126,19 +125,37 @@ export default function Profile() {
     }
   };
 
-  const handleShowListings = async()=>{
+  const handleShowListings = async () => {
     try {
       const res = await fetch(`/api/user/listings/${currentUser._id}`);
-      const data =  await res.json();
-      if(data.success === false){
-setShowListingsError(true);
-return;
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
       }
-      setUserListings(data)
+      setUserListings(data);
     } catch (error) {
-      setShowListingsError(true)
+      setShowListingsError(true);
     }
-  }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -226,42 +243,44 @@ return;
       <button onClick={handleShowListings} className="text-green-700 w-full">
         Show listings
       </button>
-      <p className="text-red-500 mt-5">{showListingsError ? "Error showing your listings" : ""}</p>
-    
+      <p className="text-red-500 mt-5">
+        {showListingsError ? "Error showing your listings" : ""}
+      </p>
+
       {userListings && userListings.length > 0 && (
-        <div className='flex flex-col gap-4'>
-          <h1 className='text-center mt-7 text-2xl font-semibold'>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
             Your Listings
           </h1>
-          
+
           {userListings.map((listing) => (
             <div
               key={listing._id}
-              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
               <Link to={`/listing/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
-                  alt='listing cover'
-                  className='h-16 w-16 object-contain'
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
                 />
               </Link>
               <Link
-                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
                 to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
               </Link>
 
-              <div className='flex flex-col item-center'>
+              <div className="flex flex-col item-center">
                 <button
-                  
-                  className='text-red-700 uppercase'
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
                 >
                   Delete
                 </button>
                 <Link to={`/update-listing/${listing._id}`}>
-                  <button className='text-green-700 uppercase'>Edit</button>
+                  <button className="text-green-700 uppercase">Edit</button>
                 </Link>
               </div>
             </div>
